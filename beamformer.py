bac920884b4ee65ab73c4547711aad09a662a7ed
Krish Pandya <kpandya@umd.edu>
2023-10-19 22:53:35 -0400

    beamformer commented code

--- beamformer.py
+++ beamformer.py
@@ -1,16 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+import warnings
+warnings.filterwarnings("ignore")
 
 plt.rcParams["figure.figsize"] = (8, 8)
 plt.rcParams["font.size"] = 14
 plt.style.use('dark_background') # dark theme
 
-
+# 2*pi*r/lambda is the phase of the emitter from the focus point x,y
 def CalculatePhaseFromFocus(x, y, e):
     return np.sqrt(np.sum((e.r-np.array([x, y]))**2))*(2*np.pi/e.lambda0)
 
-
+# Create an animated/moving emitter based on it's position (x,y), speed (c), frequecy(f),
+# phase(phi), Other variables necessary for the plotting part of the program
 class Emitter():
 
     def __init__(self, x, y, c, f, phi, rMax=100, color="tab:blue", alpha=0.6):
@@ -20,6 +23,8 @@ class Emitter():
         self.SetUp()
         self.SetPhase(phi)
 
+    # For the animation increment the time function. Also increase the size 
+    # of each circle appropriately
     def Increment(self, dt):
         self.t += dt
         if self.t < self.t0:
@@ -31,27 +36,29 @@ class Emitter():
             circle.set_width(2*r)
             circle.set_alpha(self.alpha if i < ((self.t-self.t0)/self.T) else 0)
 
+    # Set phase and an offset time(this is in order to display the animation correctly)
     def SetPhase(self, phi):
         self.phi = self.Wrap(phi, 2*np.pi)
         self.t0 = self.T*(1-self.phi/(2*np.pi))
         self.t = 0
 
+    # Setup to calculate different values necessary
     def SetUp(self):
-        self.lambda0 = self.c/self.f
+        self.lambda0 = self.c/self.f 
         self.T = 1./self.f
         self.N = np.int64(np.ceil(self.rMax/self.lambda0))
         self.circles = [plt.Circle(xy=tuple(self.r), fill=False, lw=2,
                                    radius=0, alpha=self.alpha,
                                    color=self.color)
                         for i in range(self.N)]
-
+    # In order to wrap the circles back after every iteration so that it keeps looping
     def Wrap(self, x, x_max):
         if x >= 0:
             return x - np.floor(x/x_max) * x_max
         if x < 0:
             return x_max - (-x - np.floor(-x/x_max) * x_max)
 
-
+# Array of emmiters which basically increments each emmtier
 class EmitterArray():
 
     def __init__(self):
@@ -94,7 +101,7 @@ N = 10
 
 emitter_array = EmitterArray()
 
-
+# Main code!
 # ########################################################
 # #Focussed Array
 xs = np.linspace(-lambda0, lambda0, N)
@@ -106,13 +113,20 @@ x = int(x)
 y = int(y)
 print ("Directing the emmiter to point to",x,",",y)
 for i in range(N):
+    #create an array of N emmiters on the x axis from -lambda to lambda at equal distance
     e = Emitter(xs[i], ys[i], c, f, 0)
+    # Calculate the phase for each emmiter in order to connverge on the user given point
     phase = CalculatePhaseFromFocus(x, y, e)
+    # update the emmiter's phase to that
     e.SetPhase(phase)
+    # Add the emitter to the array
     emitter_array.AddEmitter(e)
 # #######################################################
 emitter_array.RemoveOffset()
 
+
+
+# Plot the animation
 fig, ax = plt.subplots()
 ax.set_xlim([-X/2, Y/2])
 ax.set_ylim([-X/2, Y/2])
